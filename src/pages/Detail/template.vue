db1f45ac67b0b1b6afa937f2fbaa2efbdebbf457
lgj <469779397@qq.com>
2018-09-27 23:05:38 +0800

    add vue-router

---
+++ src/pages/Detail/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="detail">
+    详情页面
+  </div>
+</template>
+
+<script src = "./template.js">
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src = "./template.css">
+
+</style>
