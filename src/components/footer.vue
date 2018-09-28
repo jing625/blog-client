0a5f1e5cdd56c3d876ca2e350d9fb4232faedfcc
jing <244457187@qq.com>
2018-09-28 17:45:01 +0800

    add header and footer style

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,14 +4,14 @@
   </footer>
 </template>
 
-<!--<style scoped>-->
-<!--footer {-->
-  <!--align-self: end;-->
-  <!--background-color: #d7d7d7;-->
-  <!--color: #666;-->
-  <!--font-size: 13px;-->
-  <!--padding: 10px;-->
-  <!--text-align: center;-->
-<!--}-->
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
 
-<!--</style>-->
+</style>
