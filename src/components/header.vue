53b4bdf2f06cd455351c982ae4b3ec5cbf008c44
jing <244457187@qq.com>
2018-09-29 15:57:05 +0800

    登录功能

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,8 +4,8 @@
       <h1>Let's share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
-        <el-button><router-link to="/login">立即登录</router-link></el-button>
-        <el-button><router-link to="/register">注册账号</router-link></el-button>
+        <router-link to="/login"><el-button>立即登录</el-button></router-link>
+        <router-link to="/register"><el-button>注册账号</el-button></router-link>
       </div>
     </template>
     <template v-if="isLogin">
