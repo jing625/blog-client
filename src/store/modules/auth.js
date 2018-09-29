ef03c5fd04b4d1eb96647c36955fa7352af069d5
jing <244457187@qq.com>
2018-09-29 15:37:00 +0800

    改错后

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,46 +4,50 @@ const state = {
   user: null,
   isLogin: false
 }
+
 const getters = {
   user: state => state.user,
   isLogin: state => state.isLogin
 }
+
 const mutations = {
   setUser(state, payload) {
     state.user = payload.user
   },
+
   setLogin(state, payload) {
     state.isLogin = payload.isLogin
   }
 }
+
 const actions = {
-  login({commit}, {username, password}) {
-    return auth.login({username, password})
+  login({ commit }, { username, password }) {
+    return auth.login({ username, password })
       .then(res => {
-        commit('setUser', {user: res.data})
-        commit('setLogin', {isLogin: true})
+        commit('setUser', { user: res.data })
+        commit('setLogin', { isLogin: true })
       })
   },
 
-  async register({commit}, {username, password}) {
-    let res = await auth.register({username, password})
-    commit('setUser', {user: res.data})
-    commit('setLogin', {isLogin: true})
+  async register({ commit }, { username, password }) {
+    let res = await auth.register({ username, password })
+    commit('setUser', { user: res.data })
+    commit('setLogin', { isLogin: true })
     return res.data
   },
 
-  async logout({commit}) {
+  async logout({ commit }) {
     await auth.logout()
-    commit('setUser', {user: null})
-    commit('setLogin', {isLogin: false})
+    commit('setUser', { user: null })
+    commit('setLogin', { isLogin: false })
   },
 
-  async checkLogin({commit, state}) {
-    if (state.isLogin) return true
+  async checkLogin({ commit, state}) {
+    if(state.isLogin) return true
     let res = await auth.getInfo()
-    commit('setLogin', {isLogin: res.isLogin})
-    if (!res.isLogin) return false
-    commit('setUser', {user: res.data})
+    commit('setLogin', { isLogin: res.isLogin })
+    if(!res.isLogin) return false
+    commit('setUser', { user: res.data })
     return true
   }
 
@@ -56,8 +60,8 @@ const actions = {
 }
 
 export default {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {}
+  state,
+  getters,
+  mutations,
+  actions
 }
